f33129803311c4d3019bc4d6ad4bb6d8bb738ebb
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-07-18 18:23:50 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 
 module.exports = {
     
-    port: 2000, // Port to host website on
+    port: process.env.PORT, // Port to host website on
 
     params: {   // Always send this stuff to the servers
         secret: 'Wmfd2893gb7',
@@ -11,10 +11,7 @@ module.exports = {
         binaryVersion: '35',
         gdbrowser: '1'
     },
-
-    rateLimiting: true, // Enables rate limiting to avoid api spam, feel free to disable for private use.
     ipForwarding: true, // Forwards 'x-real-ip' to the servers. (requested by robtop)
-
     cacheMapPacks: true, // Caches map packs to speed up loading. Useful if they're rarely updated.
     cacheAccountIDs: true, // Caches account IDs in order to shave off an extra request to the servers.
     cachePlayerIcons: true, // Caches player icons to speed up loading. Changing your icon in-game may take time to update on the site.
